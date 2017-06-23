61c4f7dcb6dec37b4c4f9f88f750e6f0abd9b1e7
1410Ma <18845299725@163.com>
2017-06-23 11:23:58 +0800

    首次提交

---
+++ src/webSocketDemo/WebsoketD.java
@@ -0,0 +1,15 @@
+package webSocketDemo;
+
+import javax.websocket.OnOpen;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/webS")
+public class WebsoketD {
+	
+	
+	public void onOpen(){
+		
+	}
+	
+	
+}
